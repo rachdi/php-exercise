2562e2078e4be59c894d361d4e4510e873f8f2a1
rachdi <bigking3100@gmail.com>
2017-01-09 14:53:42 +0100

    une fonction qui prend trois paramètres : nom, prenom et age. Elle doit renvoyer une chaine de la forme : "Bonjour" + nom + prenom + ",tu as" + age + "ans".

--- function/exercice5.php
+++ function/exercice5.php
@@ -3,7 +3,7 @@
 function retourvrai(){ 
 	$i=20;
 	$k="vélo";
-	echo "Dans mon garage il y'as $i vélo";
+	echo "Dans mon garage il y'as $i $k";
 }
 retourvrai();
 ?>
---
+++ function/exercice6.php
@@ -0,0 +1,10 @@
+<?php 
+
+function retourInfo(){ 
+	$nom="Rachdi";
+	$prenom="Bassem";
+	$age=25;
+	echo "Bonjour $nom  $prenom tu as $age ans";
+}
+retourInfo();
+?>
