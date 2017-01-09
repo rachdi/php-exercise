1f6c35404c2b6701fc30281da007ac24ff506f13
rachdi <bigking3100@gmail.com>
2017-01-09 16:52:51 +0100

    tableau associatif avec comme index le numéro des départements des Hauts de France et en valeur leur nom.

---
+++ array/exercice5.php
@@ -0,0 +1,27 @@
+<?php 
+$HautFr=array(
+	"Lille"=>1,
+	"Amiens"=>2,
+	"Roubaix"=>3,
+	"Tourcoing"=>4,
+	"Dunkerque"=>5,
+	"Calais"=>6,
+	"Villeneuve-d'Ascq"=>7,
+	"Saint-Quentin"=>8,
+	"Beauvais"=>9,
+	"Valenciennes"=>10,
+	"Boulogne-sur-Mer"=>11,
+	"Wattrelos"=>12,
+	"Maubeuge"=>13,
+	"Arras"=>14,
+	"Douai"=>15,
+	"Compiègne"=>16,
+	"Marcq-en-Barœul"=>17,
+	"Creil"=>18,
+	"Cambrai"=>19,
+	"Liévin"=>20,
+	"Lens"=>21,
+	
+	);
+
+	?>
