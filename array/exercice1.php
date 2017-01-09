a2896a74549fdb70e88c45c5dcac0b5a3addf193
rachdi <bigking3100@gmail.com>
2017-01-09 16:23:43 +0100

    un tableau mois

---
+++ array/exercice1.php
@@ -0,0 +1,3 @@
+<?php 
+$mois=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+?>
--- function/exercice1.php
+++ function/exercice1.php
@@ -4,10 +4,10 @@ function retourvrai(){
 	$i=0;
 	$k=20;
 	if($i<$k === true){
-		var_dump((bool) true); 
+		var_dump((bool) 1); 
 	}
 	else{
-		var_dump((bool) false); ;
+		var_dump((bool) 0); ;
 	}
 }
 retourvrai();
