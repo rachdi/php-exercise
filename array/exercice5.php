27fe5d0b3d9eaf520b768fd64630b302f733d1d9
rachdi <bigking3100@gmail.com>
2017-01-10 10:09:13 +0100

    affichage de la valeur de l'index 59.

--- array/exercice5.php
+++ array/exercice5.php
@@ -1,28 +1,6 @@
 <?php 
-$HautFr=array(
-	59=>"Lille",
-	"Amiens"=>80,
-	"Roubaix"=>3,
-	"Tourcoing"=>4,
-	"Dunkerque"=>5,
-	"Calais"=>6,
-	"Villeneuve-d'Ascq"=>7,
-	"Saint-Quentin"=>8,
-	"Beauvais"=>9,
-	"Valenciennes"=>10,
-	"Boulogne-sur-Mer"=>11,
-	"Wattrelos"=>12,
-	"Maubeuge"=>13,
-	"Arras"=>14,
-	"Douai"=>15,
-	"Compiègne"=>16,
-	"Marcq-en-Barœul"=>17,
-	"Creil"=>18,
-	"Cambrai"=>19,
-	"Liévin"=>20,
-	"Lens"=>21,
-	
-	);
+$HautFr=array(02=>"aisne",59=>"nord",60=>"oise",62=>"pas-de-calais",80=>"somme");
+
 echo $HautFr["59"];
 
 ?>
