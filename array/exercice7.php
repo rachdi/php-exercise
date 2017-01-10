debf95d5230861a9559b736d6a43c797fd19597c
rachdi <bigking3100@gmail.com>
2017-01-10 10:58:22 +0100

    affichage de  toutes les valeurs de ce tableau 1.

--- array/exercice7.php
+++ array/exercice7.php
@@ -3,4 +3,5 @@ $HautFr=array(02=>"aisne",59=>"nord",60=>"oise",62=>"pas-de-calais",80=>"somme")
 
 $HautFr[51]="Reims";
 
+
 ?>
---
+++ array/exercice8.php
@@ -0,0 +1,7 @@
+<?php 
+$mois=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+foreach ($mois as$value) {
+	echo "$value</br>";
+
+}
+?>
