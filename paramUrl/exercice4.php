557a2c676cd32255496a6cee4968a2f69f6a677a
rachdi <bigking3100@gmail.com>
2017-01-10 13:37:08 +0100

    index.php?langage=PHP&serveur=LAMP

---
+++ paramUrl/exercice4.php
@@ -0,0 +1,19 @@
+
+<?php  
+
+
+
+$langage="PHP";
+$serveur="LAMP";
+
+if(!isset($langage) && !iseet($serveur)){
+
+
+	echo "error ";
+}
+else {
+	echo "we work with $langage on the server $serveur";
+}
+
+
+?>
--- paramUrl/index3.php
+++ paramUrl/index3.php
@@ -4,12 +4,12 @@ $dateDebut="2/05/2016";
 $dateFin="27/11/2016";
 
 
-if(isset($_GET["dateDebut"] ) && isset($_GET["dateFin"])){
-	
-	echo "we don't have the start date and the end date" ;
+if(isset($dateDebut] ) && isset($dateFin)){
+	echo "the start date is : $dateDebut and the end date is $dateFin !";
 }
 else{
-	echo "the start date is : $dateDebut and the end date is $dateFin !";
+	
+	echo "we don't have the start date and the end date" ;
 }
 
 
