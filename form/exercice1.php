28122f451838d907ce24aa78e35619832a2b5af5
rachdi <bigking3100@gmail.com>
2017-01-11 15:11:30 +0100

    methode get

---
+++ form/exercice1.php
@@ -0,0 +1,27 @@
+<?php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="user.php" method="GET">
+		<label for="nom">Nom</label>
+		<input type="text" name="user" id="nom"><br>
+		<label for="prenom">Prenom</label>
+		<input type="text" name="user" id="prenom"><br>
+		<input type="submit" value="entrez">
+
+
+
+
+
+
+
+
+	</form>
+
+</body>
+</html>
--- php-exercises-date/calender.php
+++ php-exercises-date/calender.php
@@ -3,61 +3,29 @@
 
 $mois= $_POST['mois'];
 $annee=$_POST['annee'];
-// $jour=date("N");
+
 
 $number = cal_days_in_month(CAL_GREGORIAN,$mois, $annee); // 31
 echo $number;
 
 $nom_jour= array(
-	1=>"dimanche",
-	2=>"lundi", 
-	3=> "mardi", 
-	4=> "mercredi",
-	5=> "jeudi", 
-	6=> "vendredi", 
-	7=> "samedi"
+	0=>"dimanche",
+	1=>"lundi", 
+	2=> "mardi", 
+	3=> "mercredi",
+	4=> "jeudi", 
+	5=> "vendredi", 
+	6=> "samedi"
 	);
-for($i=1; $i<$number; $i++){
-	echo "<table><th>$i</th></table>";
-
+for($i=1; $i<=$number; $i++){
 	foreach ($nom_jour as $key => $value) {
-
-		if($jour==$key){
-			echo "<table><th>$value:$i</th></table>";
-		}
+		$n="N";
+		$njour=date("$N");	
+		echo "<table><td>$value/td>";
+		echo "<th>$i</th></table>";
 	}
+
 }
-// for($i=0; $i<$number; $i++){
-// 	echo "<table><th>$value</th></table>";
-
-// }
 
 
-
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<table>
-		<td>dimanche</td>
-		<td>lundi</td>
-		<td>mardi</td>
-		<td>mercredi</td>
-		<td>jeudi</td>
-		<td>vendredi</td>
-		<td>samedi</td>
-	</table>
-	<table>
-		<th></th>
-		<th></th>
-		<th></th>
-		<th></th>
-		<th></th>
-		<th></th>
-
-	</table>
-</body>
-</html>
+?>
